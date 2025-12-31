f5671cd890edc68f71efbb6756f37e23c0c97811
Fili75 <you.example.com>
2025-12-31 13:27:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import datetime
+from datetime import datetime, timedelta
 
 st.title("Air quality sensors")
 if ("data" not in st.session_state):
@@ -12,11 +12,11 @@ if "date_min" not in st.session_state:
     st.session_state["date_min"] = datetime.strptime(st.session_state["data"]["timestamp"].min().strftime("%Y/%m/%d"),"%Y/%m/%d")
 if "date_max" not in st.session_state:
     st.session_state["date_max"] = datetime.strptime(st.session_state["data"]["timestamp"].max().strftime("%Y/%m/%d"),"%Y/%m/%d")
-
 print(st.session_state["date_min"])
 
 date_min_input = st.date_input("data inizio", value = st.session_state["date_min"], min_value= st.session_state["date_min"], max_value= st.session_state["date_max"])
 date_max_input = st.date_input("data fine", value = st.session_state["date_max"], min_value= st.session_state["date_min"], max_value= st.session_state["date_max"])
+date_max_input = date_max_input + timedelta(days=1)
 ind = (st.session_state["data"]["timestamp"] >= str(date_min_input)) & (st.session_state["data"]["timestamp"] <= str(date_max_input))
 type_data = st.selectbox("tipo dati", options = ["PM2.5", "PM10", "Tutti"])
 freq = st.selectbox("frequenza", options = ["Nessuna","Oraria", "Giornaliera", "Settimanale", "Mensile"])
