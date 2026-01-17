0b4834048b3a911da31b3eb338a2826b251613a1
Fili75 <you.example.com>
2026-01-17 10:41:18 +0100

    Update data and app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,22 +4,22 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
 st.title("Air quality sensors")
-if ("data" not in st.session_state):
-    d = pd.read_csv("./data/data_CT2.csv",sep=";")
+data_input = st.selectbox("Sensore",["CT2", "CT3","CT6","CT8"])
+if (f"data_{data_input}" not in st.session_state):
+    d = pd.read_csv(f"./data/data_{data_input}.csv",sep=";")
     d["timestamp"] = pd.to_datetime(d["timestamp"])
-    st.session_state["data"] = d
-if "date_min" not in st.session_state:
-    st.session_state["date_min"] = datetime.strptime(st.session_state["data"]["timestamp"].min().strftime("%Y/%m/%d"),"%Y/%m/%d")
+    st.session_state[f"data_{data_input}"] = d
+if f"data_{data_input}_date_min" not in st.session_state:
+    st.session_state[f"data_{data_input}_date_min"] = datetime.strptime(st.session_state[f"data_{data_input}"]["timestamp"].min().strftime("%Y/%m/%d"),"%Y/%m/%d")
 if "date_max" not in st.session_state:
-    st.session_state["date_max"] = datetime.strptime(st.session_state["data"]["timestamp"].max().strftime("%Y/%m/%d"),"%Y/%m/%d")
-print(st.session_state["date_min"])
+    st.session_state[f"data_{data_input}_date_max"] = datetime.strptime(st.session_state[f"data_{data_input}"]["timestamp"].max().strftime("%Y/%m/%d"),"%Y/%m/%d")
 
-date_min_input = st.date_input("data inizio", value = st.session_state["date_min"], min_value= st.session_state["date_min"], max_value= st.session_state["date_max"])
-date_max_input = st.date_input("data fine", value = st.session_state["date_max"], min_value= st.session_state["date_min"], max_value= st.session_state["date_max"])
+date_min_input = st.date_input("data inizio", value = st.session_state[f"data_{data_input}_date_min"], min_value= st.session_state[f"data_{data_input}_date_min"], max_value= st.session_state[f"data_{data_input}_date_max"])
+date_max_input = st.date_input("data fine", value = st.session_state[f"data_{data_input}_date_max"], min_value= st.session_state[f"data_{data_input}_date_min"], max_value= st.session_state[f"data_{data_input}_date_max"])
 date_max_input = date_max_input + timedelta(days=1)
-ind = (st.session_state["data"]["timestamp"] >= str(date_min_input)) & (st.session_state["data"]["timestamp"] <= str(date_max_input))
+ind = (st.session_state[f"data_{data_input}"]["timestamp"] >= str(date_min_input)) & (st.session_state[f"data_{data_input}"]["timestamp"] <= str(date_max_input))
 type_data = st.selectbox("tipo dati", options = ["PM2.5", "PM10", "Tutti"])
-freq = st.selectbox("frequenza", options = ["Nessuna","Oraria", "Giornaliera", "Settimanale", "Mensile"])
+freq = st.selectbox("frequenza", options = ["Nessuna","Oraria", "Giornaliera", "Settimanale", "Mensile"],index=3)
 freq_dict = {
     "Oraria": "H",
     "Giornaliera":"D",
@@ -31,14 +31,14 @@ fig, ax = plt.subplots(1,1)
 
 if type_data == "Tutti":
     for i in ["PM2.5", "PM10"]:
-        d1 = st.session_state["data"].loc[(st.session_state["data"]["Parametro"] == i) & ind]
+        d1 = st.session_state[f"data_{data_input}"].loc[(st.session_state[f"data_{data_input}"]["Parametro"] == i) & ind]
         if freq != "Nessuna":
             d1 = d1[["timestamp", "Valore"]].groupby(pd.Grouper(key="timestamp", freq= freq_dict[freq])).mean().reset_index()
 
         ax.plot(d1["timestamp"], d1["Valore"], label = i)
     ax.legend(labelcolor="w")
 else:
-    d1 = st.session_state["data"].loc[(st.session_state["data"]["Parametro"] == type_data) & ind]
+    d1 = st.session_state[f"data_{data_input}"].loc[(st.session_state[f"data_{data_input}"]["Parametro"] == type_data) & ind]
     if freq != "Nessuna":
         d1 = d1[["timestamp", "Valore"]].groupby(pd.Grouper(key="timestamp", freq=freq_dict[freq])).mean().reset_index()
     ax.plot(d1["timestamp"], d1["Valore"], label =type_data)
